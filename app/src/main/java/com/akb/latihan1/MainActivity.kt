a541bf42488712b02d31e6eeb84faddf33165915
Your Name <nizarmu7@gmail.com>
2020-04-05 11:23:34 +0700

    Memperbaiki EditText input1(kode keluarga) dan menambahkan komentar

--- app/src/main/java/com/akb/latihan1/MainActivity.kt
+++ app/src/main/java/com/akb/latihan1/MainActivity.kt
@@ -17,3 +17,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+/*
+Tanggal Pengerjaan : 2 April 2020
+Deskripsi Pengerjaan : Membuat project awal dengan membuat MainActivity dan activity_main.
+
+Tanggal Pengerjaan : 3 April 2020
+Deskripsi Pengerjaan : Membuat kelas KodeLogin,Biodata,BeresOke dan layout activity_kode_login,activity,_biodata,
+activity_beres_oke. Serta membuat validasi untuk Edittext input1(kode keluarga), dan validasi nama dan umur. dan
+membuat hasil untuk nama serta kata-katanya untuk hasil di activity_beres_oke. Dan mulai mengupload ke github.
+
+Tanggal Pengerjaan : 4 April 2020
+Deskripsi Pengerjaan : Memperbaiki inputan di edittext input1(kode keluarga) agar huruf kapital semua.
+
+NIM : 10117149
+NAMA : M NIZAR MIFTAHUL ULYA
+KELAS : IF4
+*/
